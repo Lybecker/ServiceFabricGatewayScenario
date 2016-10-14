5ef78bf85c8cc88a55082adc40f944d4652fbc86
Rasmus Svendsen <rasmus.svendsen@gmail.com>
2016-10-14 14:27:34 +0200

    Add code for testing TcpClient and using BeginConnect

--- Gateway.Service/Controllers/ValuesController.cs
+++ Gateway.Service/Controllers/ValuesController.cs
@@ -68,6 +68,40 @@ namespace Gateway.Service.Controllers
             return result;
         }
 
+        [HttpGet]
+        public async Task<string> Tcp()
+        {
+            string result;
+            int attempts = 0;
+            int timeoutExceptions = 0;
+            DateTime startTime = DateTime.UtcNow;
+            try
+            {
+                var serviceUri = new ServiceUriBuilder("AppService", "StatelessWebApi").ToUri();
+                ServicePartitionClient<HttpCommunicationClient> partitionClient = new ServicePartitionClient<HttpCommunicationClient>(communicationFactory, serviceUri);
+
+                result = await partitionClient.InvokeWithRetryAsync(async client =>
+                {
+                    attempts++;
+                    string innerResult;
+                    try
+                    {
+                        innerResult = await HttpRequestUsingTcpClientAsync(client.Url.Host, client.Url.Port, "/api/stateless");
+                    }
+                    catch (Exception)
+                    {
+                        timeoutExceptions++;
+                        throw;
+                    }
+                    return innerResult;
+                });
+            }
+            catch (Exception e)
+            {
+                result = $"Error: {e.Message} - {e.StackTrace}";
+            }
+            return $"Attempts: {attempts} - TimeoutsInTcp: {timeoutExceptions} - Took: {DateTime.UtcNow.Subtract(startTime).TotalMilliseconds.ToString("0.####")} ms - {result}";
+        }
 
         // Client from https://github.com/xinyanmsft/SFStartupHttp/tree/TestA
         [HttpGet]
@@ -94,6 +128,99 @@ namespace Gateway.Service.Controllers
                                     new HttpServiceClientStatusCodeRetryHandler(
                                         new HttpTraceMessageHandler(null))));
             return new HttpClient(handler);
+        }
+
+        private static async Task<string> HttpRequestUsingTcpClientAsync(string host = "baoanders.westeurope.cloudapp.azure.com", int port = 8127,
+            string urlPath = "/api/values/long?sec=5")
+        {
+            string result;
+
+            using (var tcp = new TcpClient())
+            {
+                IAsyncResult ar = tcp.BeginConnect(host, port, null, null);
+                System.Threading.WaitHandle wh = ar.AsyncWaitHandle;
+                try
+                {
+                    if (!ar.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1), false))
+                    {
+                        tcp.Close();
+                        throw new TimeoutException("Failed to connect to server");
+                    }
+
+                    using (var stream = tcp.GetStream())
+                    {
+                        //tcp.SendTimeout = 500;
+                        //tcp.ReceiveTimeout = 1000;
+
+                        // Send request headers
+                        var builder = new StringBuilder();
+                        builder.AppendLine($"GET {urlPath} HTTP/1.1");
+                        builder.AppendLine($"Host: {host}:{port}");
+                        //builder.AppendLine("Content-Length: " + data.Length);   // only for POST request
+                        builder.AppendLine("Connection: close");
+                        builder.AppendLine();
+                        var header = Encoding.ASCII.GetBytes(builder.ToString());
+                        await stream.WriteAsync(header, 0, header.Length);
+
+                        // Send payload data if you are POST request
+                        //await stream.WriteAsync(data, 0, data.Length);
+
+                        // receive data
+                        using (var memory = new MemoryStream())
+                        {
+                            await stream.CopyToAsync(memory);
+                            memory.Position = 0;
+                            var data = memory.ToArray();
+
+                            var index = BinaryMatch(data, Encoding.ASCII.GetBytes("\r\n\r\n")) + 4;
+                            var headers = Encoding.ASCII.GetString(data, 0, index);
+                            memory.Position = index;
+
+                            if (headers.IndexOf("Content-Encoding: gzip") > 0)
+                            {
+                                using (GZipStream decompressionStream = new GZipStream(memory, CompressionMode.Decompress))
+                                using (var decompressedMemory = new MemoryStream())
+                                {
+                                    decompressionStream.CopyTo(decompressedMemory);
+                                    decompressedMemory.Position = 0;
+                                    result = Encoding.UTF8.GetString(decompressedMemory.ToArray());
+                                }
+                            }
+                            else
+                            {
+                                result = Encoding.UTF8.GetString(data, index, data.Length - index);
+                            }
+                        }
+                    } //End TCP.GetStream()
+                }
+                finally
+                {
+                    wh.Close();
+                }
+            }
+            return result;
+        }
+
+        private static int BinaryMatch(byte[] input, byte[] pattern)
+        {
+            int sLen = input.Length - pattern.Length + 1;
+            for (int i = 0; i < sLen; ++i)
+            {
+                bool match = true;
+                for (int j = 0; j < pattern.Length; ++j)
+                {
+                    if (input[i + j] != pattern[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
